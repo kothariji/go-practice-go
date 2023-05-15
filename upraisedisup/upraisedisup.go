9276b53c5fda3c261d69e85192b28f5f1ba09c0d
kothariji <kotharidhruv25@gmail.com>
2023-05-15 09:28:33 +0530

    add channel

--- upraisedisup/upraisedisup.go
+++ upraisedisup/upraisedisup.go
@@ -3,9 +3,12 @@ package upraisedisup
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func CheckUpraised() {
+	startTime := time.Now()
+
 	links := []string{
 		"https://www.upraised.co/",
 		"https://www.upraised.co/app/",
@@ -13,19 +16,38 @@ func CheckUpraised() {
 		"https://hire.upraised.co/",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	// receiving response msgs via channel
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	//checking infinitely
+	for l := range c {
+		go checkLink(l, c)
+	}
+
+	fmt.Println("Total time taken for execution: ", time.Since(startTime))
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
+	result := ""
 	if err != nil {
-		fmt.Println(err)
-		fmt.Printf("Oops! %v is down, please check \n", link)
+		result = fmt.Sprintf("Oops! %v is down, please check \n", link)
+		fmt.Printf(result)
+		c <- link
 		return
 	}
-	fmt.Printf("Yahoo! %v is up and running, chai break ? \n", link)
+	result = fmt.Sprintf("Yahoo! %v is up and running, chai break ? \n", link)
+	fmt.Printf(result)
+	c <- link
+	return
 
 }
